0ea3d1f9020ebc3cc890b20e28de65fcdd27b909
Kayode Odole <31928403+Kayconfig@users.noreply.github.com>
2025-10-09 06:44:03 +0100

    feat: enhance createHabitSchema to include tagsId for better habit categorization

--- src/habit/schemas/create-habit.dto.ts
+++ src/habit/schemas/create-habit.dto.ts
@@ -1,10 +1,22 @@
-import { number, object, string, type infer as ZodInfer } from 'zod';
+import {
+  array,
+  number,
+  object,
+  string,
+  union,
+  uuid,
+  uuidv4,
+  uuidv6,
+  uuidv7,
+  type infer as ZodInfer,
+} from 'zod';
 
 export const createHabitSchema = object({
   name: string().nonempty(),
   frequency: string().nonempty(),
   description: string().optional().default(''),
   targetCount: number().optional(),
+  tagsId: array(union([uuid(), uuidv4(), uuidv6(), uuidv7()])).optional(),
 });
 
 export type CreateHabit = ZodInfer<typeof createHabitSchema>;
